d672802a6a376f291e3ca500d0d1f2756a6652a0
Bruno Moura Dourado <brunomouradourado@gmail.com>
2021-03-11 21:14:50 +0100

    Added dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { PlayerModule } from './player/player.module';
 import { CategoryModule } from './category/category.module';
 import { ChallengeModule } from './challenge/challenge.module';
 
+
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb+srv://admin:HNe4Q!BYx!5AiNy@cluster0.r56hi.mongodb.net/smart-ranking?retryWrites=true&w=majority`,
+      process.env.MONGO_CONNECTION_STRING,
       { useNewUrlParser:true, useCreateIndex:true, useFindAndModify:false }
     ),
     PlayerModule,
